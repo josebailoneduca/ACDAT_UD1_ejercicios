0fa2653a74c59b169d03ef6d6abe0f3fda5bb9c3
Jose Bailon <josebailoneduca@gmail.com>
2023-10-31 18:41:38 +0100

    Ejercicio 12 terminado

--- src/ejercicio12/Ejercicio12.java
+++ src/ejercicio12/Ejercicio12.java
@@ -18,6 +18,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +29,7 @@ import java.util.logging.Logger;
  */
 public class Ejercicio12 {
 
-    //ATRIBUTOS
+    //ATRIBUTOS####################################################
     /**
      * Scanner de consola
      */
@@ -39,34 +40,37 @@ public class Ejercicio12 {
      */
     String ruta = "./src/ejercicio12/recursos/ListaTrabajos.dat";
 
+    int trabajosExistentes = 0;
+
+    //METODOS###########################################################
     /**
      * Constructor. Agrega los datos iniciales al archivo y muestra el menu
      */
     public Ejercicio12() {
+        trabajosExistentes = contarTrabajosEnDisco();
         agregarIniciales();
         menu();
     }
 
-    //METODOS
     /**
      * Agregar datos iniciales si no hay trabajos en el archivo
      */
     private void agregarIniciales() {
-        //si ya tiene contenido no se agrega el inicial
-        if (leerTrabajosDeDisco(false)) {
+        //si ya tiene contenido no se agregan los datos iniciales
+        if (trabajosExistentes > 0) {
             return;
         }
-        //T1
-        escribirTrabajo(new Trabajo("T1", "1/8/2023", 2));
+        //escribir T1
+        escribirTrabajo(new Trabajo("T1", "01/08/2023", 2));
         escribirEmpleado(new Empleado("Pepe", "Pepillos", 2000));
         escribirEmpleado(new Empleado("Juan", "Juanillos", 2000));
-        //T2
-        escribirTrabajo(new Trabajo("T2", "15/8/2023", 3));
+        //escribir T2
+        escribirTrabajo(new Trabajo("T2", "15/08/2023", 3));
         escribirEmpleado(new Empleado("Antonio", "Antonillos", 1700));
         escribirEmpleado(new Empleado("Laura", "Laurillas", 1700));
         escribirEmpleado(new Empleado("Jose", "Joselillos", 1500));
-        //T3
-        escribirTrabajo(new Trabajo("T3", "1/9/2023", 2));
+        //escribir T3
+        escribirTrabajo(new Trabajo("T3", "01/09/2023", 2));
         escribirEmpleado(new Empleado("Sandra", "Sandrilla", 1450));
         escribirEmpleado(new Empleado("Rodrigo", "Rodriguillo", 1450));
     }
@@ -77,15 +81,21 @@ public class Ejercicio12 {
      */
     private void menu() {
         while (true) {
+            //recoger opcion del menu elegida
             int opcionElegida = this.mostrarMenu();
             switch (opcionElegida) {
                 //Leer todos los trabajos del disco
                 case 1 -> {
                     pasarPagina();
-                    leerTrabajosDeDisco(true);
+                    imprimirTrabajosDeDisco(null);
+                }
+                //Listar empleados de un trabajo
+                case 2 -> {
+                    pasarPagina();
+                    listarEmpleadosDeUnTrabajo();
                 }
                 //introducir trabajo en archivo
-                case 2 -> {
+                case 3 -> {
                     pasarPagina();
                     pedirDatosTrabajo();
                 }
@@ -97,9 +107,23 @@ public class Ejercicio12 {
     }
 
     /**
+     * Lista los empleados de un trabajo
+     */
+    private void listarEmpleadosDeUnTrabajo() {
+        //pedir el nombre a buscar
+        String nombreTrabajo = "";
+        while (nombreTrabajo.equals("")) {
+            System.out.println("Introduzca el nombre de un trabajo para listar sus empleados: ");
+            nombreTrabajo = s.nextLine();
+        }
+        //ordenar la impresion del trabajo 
+        imprimirTrabajosDeDisco(nombreTrabajo);
+    }
+
+    /**
      * Pide los datos para un trabajo. Seguidamente pide los datos de los
      * empleados. Una vez tiene los datos de los empleados crea el objeto
-     * Trabajo Por ultimo lanza el grabado a disco del trabajo y de los
+     * Trabajo. Por ultimo lanza el grabado a disco del trabajo y de los
      * empleados
      */
     private void pedirDatosTrabajo() {
@@ -109,29 +133,35 @@ public class Ejercicio12 {
         System.out.println("Introducir trabajo");
         System.out.println("*****************");
 
-        //recoger nombre
-        System.out.print("Nombre: ");
-        String nombreTrabajo = s.nextLine();
+        //Crear nombre del trabajo con formato T + (numero de trabajos existentes +1)
+        String nombreTrabajo = "T" + (trabajosExistentes + 1);
 
-        //recoger fecha
+        //recoger fecha reintentando hasta que el usuario introduce una fecha legible y aceptada
         String fecha = "";
         boolean fechaRecogida = false;
         while (!fechaRecogida) {
             try {
-                System.out.print("Fecha (dia/mes/año): ");
+                //pedir fecha al usuario
+                System.out.print("Fecha del trabajo (dia/mes/año): ");
                 DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                df.setLenient(false);
                 String tmpFecha = s.nextLine();
-                //si el string introducido no es legible por el formato saltara una excepcion
-                //Si es legible recalcula la fecha segun los valores introducidos sumando los excedentes a los limites
-                //por ejemplo si se escribe 1/13/2020 pondra el 1/1/2021 al escribir un mes mas allá del 12
-                fecha = df.format(df.parse(tmpFecha));
-                //comprobar que el usuario está conforme con la fecha entendida
-                System.out.println("¿La fecha " + fecha + " es correcta?(s/n)");
-                String acepta = s.nextLine();
-                if (acepta.toLowerCase().equals("s")) {
-                    fechaRecogida = true;
+                //Si el string introducido no es legible por el formato saltara una excepcion y se volvera a pedir
+                //si el string introducio es de una fecha posterior a la actual la volvera a pedir
+                Date fechaTemp = df.parse(tmpFecha);
+                if (fechaTemp.after(new Date(System.currentTimeMillis()))) {
+                    pedirIntro("Fecha incorrecta al ser posterior a la actual");
+                    continue;
                 }
+
+                //Si es legible recalcula la fecha segun los valores introducidos sumando los excedentes a los limites
+                //por ejemplo si el usuario escribe 1/13/2020 pondra el 1/1/2021 al escribir un mes mas allá del 12
+                fecha = df.format(fechaTemp);
+
+                fechaRecogida = true;
+
             } catch (ParseException ex) {
+                pedirIntro("Formato de fecha incorrecto");
             }
         }
 
@@ -181,6 +211,11 @@ public class Ejercicio12 {
         }
     }
 
+    /**
+     * Escribe un trabajo a disco.
+     *
+     * @param trabajo El trabajo a escribir
+     */
     private void escribirTrabajo(Trabajo trabajo) {
         File f = new File(ruta);
         FileOutputStream fos = null;
@@ -188,20 +223,24 @@ public class Ejercicio12 {
 
         //ESCRIBIR TRABAJO
         try {
-            //TRABAJO
-            boolean archivoTieneDatos = leerTrabajosDeDisco(false);
-            //preparacion de streams
+            //preparar streams
             fos = new FileOutputStream(f, true);
-            //comprobar si ya hay trabajos
-            if (!archivoTieneDatos) {
-                //Si no hay trabajos crear un objectoutputstream normal
+            //Comprobar si ya hay trabajos para decidir que tipo de objectOutputStream crear
+            if (trabajosExistentes == 0) {
+                //SI NO HAY TRABAJOS CREA UN OBJECTOUTPUTSTREAM NORMAL
                 oos = new ObjectOutputStream(fos);
             } else {
-                //Si ya hay trabajos crear un AgregarObjectOuputStream que evita la escritura de cabecera
+                //SI YA HAY TRABAJOS CREA UN AGREGAROBJECTOUTPUTSTREAM QUE EVITA LA ESCRITURA DE CABECERA
                 oos = new AgregarObjectOutputStream(fos);
             }
+
             //escribir el trabajo en disco
             oos.writeObject(trabajo);
+
+            //incrementar el numero de trabajos que hay en disco
+            trabajosExistentes++;
+
+            //tratamiento de excepciones y cierre de streams
         } catch (FileNotFoundException ex) {
             System.out.println("Archivo no encontrado " + f.getAbsolutePath());
         } catch (IOException ex) {
@@ -227,11 +266,11 @@ public class Ejercicio12 {
      */
     private void escribirEmpleado(Empleado empleado) {
 
-        //Escribir
         File f = new File(ruta);
         FileOutputStream fos = null;
         ObjectOutputStream oos = null;
-        //ESCRIBIR TRABAJO
+
+        //ESCRIBIR EMPLEADO
         try {
             //preparacion de streams
             fos = new FileOutputStream(f, true);
@@ -239,6 +278,8 @@ public class Ejercicio12 {
 
             //escribir el trabajo en disco
             oos.writeObject(empleado);
+
+            //tratamiento de excepciones y cierre de streams
         } catch (FileNotFoundException ex) {
             System.out.println("Archivo no encontrado " + f.getAbsolutePath());
         } catch (IOException ex) {
@@ -258,18 +299,23 @@ public class Ejercicio12 {
     }
 
     /**
-     * Lee los trabajos y empleados de disco.
+     * Lee los trabajos y empleados de disco. Conforme los lee puede imprimirlos
+     * o no segun se seleccione. Se puede especificar un nombre de trabajo que
+     * se pretende leer. En ese caso solo se imprimira ese trabajo
      *
-     * @param imprimir True imprimir los trabajos
-     * @return true si hay algun trabajo, false si no lo hay
+     * @param nombreTrabajo Filtro del trabajo a imprimir por su nombre. Null si
+     * no se quiere filtro
+     *
+     * @return el numero de trabajos que ya existen
      */
-    private boolean leerTrabajosDeDisco(boolean imprimir) {
+    private int imprimirTrabajosDeDisco(String nombreTrabajo) {
+
         int cantidadDeTrabajos = 0;
 
         File f = new File(ruta);
         //si no existe el archivo retorna lista vacia
         if (!f.exists()) {
-            return false;
+            return 0;
         }
 
         FileInputStream fis = null;
@@ -279,27 +325,45 @@ public class Ejercicio12 {
             //preparacion de streams
             fis = new FileInputStream(f);
             ois = new ObjectInputStream(fis);
+
             //recoger trabajos hasta EOF exception
             while (true) {
 
+                //recoger trabajo
                 Trabajo t = (Trabajo) ois.readObject();
                 cantidadDeTrabajos++;
-                if (imprimir) {
+
+                //filtro para trabajos
+                //True si el nombreTrabajo es null o coincide con el trabajo leido
+                boolean filtroTrabajosOK = nombreTrabajo == null || nombreTrabajo.equals(t.getNombre());
+
+                //imprimir trabajo si toca
+                if (filtroTrabajosOK) {
                     System.out.println("*********************************************");
                     System.out.println(t);
                 }
 
+                //recoger empleados
                 for (int i = 0; i < t.getNumsEmpleados(); i++) {
                     Empleado e = (Empleado) ois.readObject();
-                    if (imprimir) {
+
+                    //imprimir empleado si toca
+                    if (filtroTrabajosOK) {
                         System.out.println(e);
                     }
                 }
 
-            }
+                //si pasa el filtro no nulo de trabajos es que se ha encontrado el trabajo 
+                //por lo que rompemos el while
+                if (nombreTrabajo != null && filtroTrabajosOK) {
+                    break;
+                }
+            }//fin while
+
+            //tratamiento de excepciones y cierre de streams
         } catch (EOFException eofe) {
         } catch (IOException ex) {
-            return false;
+            return 0;
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Ejercicio12.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -314,11 +378,69 @@ public class Ejercicio12 {
                 Logger.getLogger(Ejercicio12.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        if (imprimir) {
-            pedirIntro("");
-        }
-        return cantidadDeTrabajos > 0;
 
+        //pedir pulsar intro tras terminar la impresion
+        pedirIntro("");
+
+        //retorna true si hay trabajos y false si no lo hay
+        return cantidadDeTrabajos;
+    }
+
+    /**
+     * Cuenta los trabajos que hay en el disco
+     *
+     * @return el numero de trabajos que ya existen
+     */
+    private int contarTrabajosEnDisco() {
+        
+        //cantidad de trabajos que hay en disco
+        int cantidadDeTrabajos = 0;
+
+        File f = new File(ruta);
+        //si no existe el archivo retorna lista vacia
+        if (!f.exists()) {
+            return 0;
+        }
+
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+
+        try {
+            //preparacion de streams
+            fis = new FileInputStream(f);
+            ois = new ObjectInputStream(fis);
+
+            //recoger trabajos hasta EOF exception
+            while (true) {
+                //recoger trabajo y aumentar la cuenta
+                Trabajo t = (Trabajo) ois.readObject();
+                cantidadDeTrabajos++;
+
+                //recorrer empleados
+                for (int i = 0; i < t.getNumsEmpleados(); i++) {
+                    ois.readObject();
+                }
+            }//fin while
+            //tratamiento de excepciones y cierre de streams
+        } catch (EOFException eofe) {
+        } catch (IOException ex) {
+            return 0;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Ejercicio12.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Ejercicio12.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        //retorna true si hay trabajos y false si no lo hay
+        return cantidadDeTrabajos;
     }
 
 //UTILES DE ENTRADA DE DATOS POR CONSOLA Y MENU
@@ -329,14 +451,15 @@ public class Ejercicio12 {
      */
     private int mostrarMenu() {
         int opcion = -1;
-        while (opcion < 1 || opcion > 3) {
+        while (opcion < 1 || opcion > 4) {
             this.pasarPagina();
             System.out.println("**************************************************");
             System.out.println("*                    OPCIONES                    *");
             System.out.println("**************************************************");
             System.out.println("1)Leer todos los trabajos");
-            System.out.println("2)Introducir un nuevo trabajo");
-            System.out.println("3)Salir");
+            System.out.println("2)Listar empleados de un trabajo");
+            System.out.println("3)Introducir un nuevo trabajo");
+            System.out.println("4)Salir");
             System.out.println("Elije una opcion:");
             try {
                 String respuesta = this.s.nextLine();
@@ -372,6 +495,11 @@ public class Ejercicio12 {
         return this.s.nextLine();
     }
 
+    /**
+     * INICIO DEL PROGRAMA
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         new Ejercicio12();
     }
